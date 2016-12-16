12267e09f5f7f79c6456ac511ba7765714132201
Matteo Padovano <mrba7ista@gmail.com>
2016-12-16 17:41:28 +0100

    add example tests

--- test/allow-extended-operators.test.js
+++ test/allow-extended-operators.test.js
@@ -8,32 +8,11 @@
 const DataSource = require('..').DataSource;
 const should = require('should');
 
-describe('Model.settings.allowExtendedOperators', () => {
-  context('DAO.find()', () => {
-    it('converts extended operators to string value by default', () => {
-      const TestModel = createTestModel();
-      return TestModel.find(extendedQuery()).then((results) => {
-        should(results[0].value).eql('[object Object]');
-      });
-    });
+describe('allowExtendedOperators', () => {
 
-    it('preserves extended operators wit allowExtendedOperators set', () => {
-      const TestModel = createTestModel({allowExtendedOperators: true});
-      return TestModel.find(extendedQuery()).then((results) => {
-        should(results[0].value).eql({$exists: true});
-      });
-    });
-
-    function extendedQuery() {
-      // datasource modifies the query,
-      // we have to build a new object for each test
-      return {where: {value: {$exists: true}}};
-    }
-  });
-
-  function createTestModel(connectorSettings) {
+  function createTestModel(connectorSettings, modelSettings) {
     const ds = createTestDataSource(connectorSettings);
-    return ds.createModel('TestModel', {value: String});
+    return ds.createModel('TestModel', {value: String}, modelSettings);
   }
 
   function createTestDataSource(connectorSettings) {
@@ -51,6 +30,14 @@ describe('Model.settings.allowExtendedOperators', () => {
     constructor(dataSource) {
     }
 
+    create(model, data, options, callback) {
+      callback();
+    }
+
+    updateAttributes(model, id, data, options, callback) {
+      callback();
+    }
+
     all(model, filter, options, callback) {
       // return the raw "value" query
       var instanceFound = {
@@ -59,4 +46,89 @@ describe('Model.settings.allowExtendedOperators', () => {
       callback(null, [instanceFound]);
     }
   }
+
+  describe('connector.settings.allowExtendedOperators', () => {
+    context('DAO.find()', () => {
+      it('converts extended operators to string value by default', () => {
+        const TestModel = createTestModel();
+        return TestModel.find(extendedQuery()).then((results) => {
+          should(results[0].value).eql('[object Object]');
+        });
+      });
+
+      it('preserves extended operators with allowExtendedOperators set', () => {
+        const TestModel = createTestModel({allowExtendedOperators: true});
+        return TestModel.find(extendedQuery()).then((results) => {
+          should(results[0].value).eql({$exists: true});
+        });
+      });
+
+      function extendedQuery() {
+        // datasource modifies the query,
+        // we have to build a new object for each test
+        return {where: {value: {$exists: true}}};
+      }
+    });
+
+    context('DAO.updateAttributes()', () => {
+      it.skip('`options.allowExtendedOperators` override connector settings - disabled', () => {
+        const TestModel = createTestModel({allowExtendedOperators: true});
+        // There is issue for update data.
+        // See https://github.com/strongloop/loopback-connector-mongodb/issues/157
+      });
+
+      it.skip('`options.allowExtendedOperators` override connector settings - enabled', () => {
+        // Default value for allowExtendedOperators is false
+        const TestModel = createTestModel({allowExtendedOperators: false});
+      });
+
+      it.skip('`Model.settings.allowExtendedOperators` override connector settings - enabled', () => {
+        const TestModel = createTestModel({allowExtendedOperators: false}, {allowExtendedOperators: true});
+      });
+
+      it.skip('`Model.settings.allowExtendedOperators` override connector settings - disabled', () => {
+        const TestModel = createTestModel({allowExtendedOperators: true}, {allowExtendedOperators: false});
+      });
+    });
+  });
+
+  describe('Model.settings.allowExtendedOperators', () => {
+    context('DAO.updateAttributes()', () => {
+      it.skip('`options.allowExtendedOperators` override Model settings - disabled', () => {
+        const TestModel = createTestModel({}, {allowExtendedOperators: true});
+      });
+
+      it.skip('`options.allowExtendedOperators` override Model settings - enabled', () => {
+        const TestModel = createTestModel({}, {allowExtendedOperators: false});
+      });
+
+      it.skip('`connector.settings.allowExtendedOperators` honor Model settings - enabled', () => {
+        const TestModel = createTestModel({allowExtendedOperators: false}, {allowExtendedOperators: true});
+      });
+
+      it.skip('`connector.settings.allowExtendedOperators` honor Model settings - disabled', () => {
+        const TestModel = createTestModel({allowExtendedOperators: true}, {allowExtendedOperators: false});
+      });
+    });
+  });
+
+  describe('options.allowExtendedOperators', () => {
+    context('DAO.updateAttributes()', () => {
+      it.skip('`Model.settings.allowExtendedOperators` honor options settings - enabled', () => {
+        const TestModel = createTestModel({}, {allowExtendedOperators: false});
+      });
+
+      it.skip('`Model.settings.allowExtendedOperators` honor options settings - disabled', () => {
+        const TestModel = createTestModel({}, {allowExtendedOperators: true});
+      });
+
+      it.skip('`connector.settings.allowExtendedOperators` honor options settings - enabled', () => {
+        const TestModel = createTestModel();
+      });
+
+      it.skip('`connector.settings.allowExtendedOperators` honor options settings - disabled', () => {
+        const TestModel = createTestModel({allowExtendedOperators: true});
+      });
+    });
+  });
 });
